c3f99341a3854180fd81a18bb86138114ab04e40
xiaohan <xiaohan2012@gmail.com>
2013-07-24 16:00:23 +0800

    graph can be made now + software requirement needs to be made

--- graph.py
+++ graph.py
@@ -3,46 +3,29 @@ from __future__ import division
 
 import networkx as nx
 
-from source import load_ing_prob_table_from_pickle
-
-
-def make_graph(tbl):
-    """(IngProbTable) => Graph"""
+def make_graph(pmi, gml_path = "data/douguo.gml"):
+    """(dict) => Graph"""
     #init graph
     g = nx.Graph()
 
     #get list of (ig1, ig2, pcm value, the weight)
-    pair_with_weights = map(lambda pair: (pair[0], pair[1], tbl[pair]), tbl.pairs)
+    pair_with_weights = map(lambda key: (key[0], key[1], pmi[key]), pmi.keys())
     
     #add the pairs to the graph
     g.add_weighted_edges_from(pair_with_weights)
 
+    nx.write_gml(g, gml_path)
+    print "to gml"
+    
+    #nx.write_gpickle(g, gml_path.split('.')[0] + '.gpickle')
+    #print "to gpickle"
+    
     return g
 
-def prob_tbl_pickle_to_gml(tbl_path="data/douguo.pickle", gml_path="data/douguo.gml"):
-    tbl = load_ing_prob_table_from_pickle(tbl_path)
-    print "finished loading pickle"
-
-    g = make_graph(tbl)
-    print "made graph"
-
-    nx.write_gml(g, gml_path)
-    print "to file"
-    
-    nx.write_gpickle(g, gml_path.split('.')[0] + '.gpickle')
-    print "to gpickle"
-
 def main():
-    
-    prob_tbl_pickle_to_gml("data/douguo.pickle", "data/test/douguo.gml")
-    
-    """
-    import jieba
-
-    g = nx.read_gpickle("data/test/douguo.gpickle")
-    for n in g.nodes():
-        print "/ ".join(jieba.cut(n.encode("utf8"), cut_all=False))
-    """
+    from cPickle import load
+    pmi = load(open("data/pmi.pickle"))
+    make_graph(pmi)
 
 if __name__ == "__main__":
     main()
